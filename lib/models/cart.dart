43d1ea969ca1d32c79427b60ab8d5bf4f8db17c3
Arpit Verma <arpitverma2008@gmail.com>
2023-04-03 00:12:45 +0530

    day29_not working

--- lib/models/cart.dart
+++ lib/models/cart.dart
@@ -3,7 +3,6 @@ import 'package:flutter_catalog/models/catalog.dart';
 import 'package:velocity_x/velocity_x.dart';
 
 class CartModel {
-   
   // catalog field
   late CatalogModel _catalog;
 
@@ -11,7 +10,7 @@ class CartModel {
   final List<int> _itemIds = [];
 
   // Get Catalog
-  CatalogModel get catalog => _catalog;
+   CatalogModel get catalog => _catalog;
 
   set catalog(CatalogModel newCatalog) {
     assert(newCatalog != null);
@@ -24,17 +23,7 @@ class CartModel {
   // Get total price
   num get totalPrice =>
       items.fold(0, (total, current) => total + current.price);
-
-
-
-
-  // Remove Item
-
-  void remove(Item item) {
-    _itemIds.remove(item.id);
-  } 
 }
-
 
 class AddMutation extends VxMutation<MyStore> {
   final Item item;
@@ -43,5 +32,15 @@ class AddMutation extends VxMutation<MyStore> {
   @override
   perform() {
     store!.cart._itemIds.add(item.id);
+  }
+}
+
+class RemoveMutation extends VxMutation<MyStore> {
+  final Item item;
+
+  RemoveMutation(this.item);
+  @override
+  perform() {
+    store!.cart._itemIds.remove(item.id);
   }
 }
--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -32,11 +32,17 @@ class _CartTotal extends StatelessWidget {
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: [
-          "\$${_cart.totalPrice}"
-              .text
-              .xl5
-              .color(context.theme.accentColor)
-              .make(),
+          VxBuilder(
+            mutations: {RemoveMutation},
+            // ignore: non_constant_identifier_names
+            builder: (context, _, VxStatus) {
+              return "\$${_cart.totalPrice}"
+                  .text
+                  .xl5
+                  .color(context.theme.colorScheme.secondary)
+                  .make();
+            },
+          ),
           30.widthBox,
           ElevatedButton(
             onPressed: () {
@@ -58,6 +64,7 @@ class _CartTotal extends StatelessWidget {
 class _CartList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    VxState.watch(context, on: [RemoveMutation]);
     final CartModel _cart = (VxState.store as MyStore).cart;
     return _cart.items.isEmpty
         ? "Nothing to show".text.xl3.makeCentered()
@@ -67,13 +74,11 @@ class _CartList extends StatelessWidget {
               leading: Icon(Icons.done),
               trailing: IconButton(
                 icon: Icon(Icons.remove_circle_outline),
-                onPressed: () {
-                  _cart.remove(_cart.items[index]);
-                  // setState(() {});
-                },
+                onPressed: () => RemoveMutation(_cart.items[index]),
               ),
               title: _cart.items[index].name.text.make(),
             ),
           );
   }
 }
+
